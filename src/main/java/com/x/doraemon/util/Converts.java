f6fc7893f41819ee9b428cd0523b8fb853f45ac7
36654 <wuchunquan@longshine.com>
2021-02-01 18:04:43 +0800

    -

--- src/main/java/com/x/doraemon/util/Converts.java
+++ src/main/java/com/x/doraemon/util/Converts.java
@@ -2,11 +2,9 @@ package com.x.doraemon.util;
 
 import com.x.doraemon.bean.New;
 import com.x.doraemon.bean.SB;
+import com.x.doraemon.interfaces.IConverter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.math.BigDecimal;
 
 import static com.x.doraemon.util.ArrayHelper.EMPTY_BYTE;
@@ -19,14 +17,29 @@ import static com.x.doraemon.util.Strings.HEX;
  * @Author AD
  */
 public class Converts {
-    
+
     // ------------------------ 变量定义 ------------------------
-    
+
     // ------------------------ 构造方法 ------------------------
-    public Converts() {}
-    
+    private Converts() {
+    }
+
     // ------------------------ 方法定义 ------------------------
-    
+
+    public static <FROM, TO> TO convert(IConverter<FROM, TO> converter, FROM from) throws Exception {
+        return converter.convert(from);
+    }
+
+    public static <FROM, TO> FROM reconvert(IConverter<FROM, TO> converter, TO to) throws Exception {
+        return converter.reconvert(to);
+    }
+
+    /**
+     * int数组转为字符串
+     *
+     * @param ints int数组
+     * @return 如：1, 2, 3
+     */
     public static String arrayToString(int[] ints) {
         if (ArrayHelper.isEmpty(ints)) {
             return "";
@@ -38,7 +51,13 @@ public class Converts {
             return sb.deleteLast().toString();
         }
     }
-    
+
+    /**
+     * 将long型数组转为字符串
+     *
+     * @param longs long数组
+     * @return 如：1, 2, 3
+     */
     public static String arrayToString(long[] longs) {
         if (ArrayHelper.isEmpty(longs)) {
             return "";
@@ -50,12 +69,39 @@ public class Converts {
             return sb.deleteLast().toString();
         }
     }
-    
+
+    /**
+     * 将字节转为16进制字符串（大写）
+     *
+     * @param b 字节数据
+     * @return 如：0A
+     */
     public static String byteToHex(byte b) {
-        String hex = Integer.toHexString(b & 255).toUpperCase().trim();
+        String hex = Integer.toHexString(b & 0xFF).toUpperCase().trim();
         return hex.length() < 2 ? "0" + hex : hex;
     }
-    
+
+    /**
+     * 将16进制字符串解析为byte值，字符串长度不能超过2
+     *
+     * @param hex 16进制字符串
+     * @return
+     */
+    public static byte hexToByte(String hex) {
+        if (hex.length() > 2) {
+            hex = hex.substring(0, 2);
+        }
+        String fix = fixHex(hex);
+        int i = Integer.parseInt(fix, 16);
+        return (byte) i;
+    }
+
+    /**
+     * 将字节数组转为16进制字符串（大写）
+     *
+     * @param bs 字节数组
+     * @return 如：0A1B
+     */
     public static String bytesToHex(byte[] bs) {
         SB sb = New.sb();
         for (byte b : bs) {
@@ -63,18 +109,13 @@ public class Converts {
         }
         return sb.toString();
     }
-    
-    public static String bytesToHexWithDelimiter(byte[] bs, String delimiter) {
-        if (ArrayHelper.isEmpty(bs)) {
-            return "";
-        }
-        SB sb = New.sb();
-        for (byte b : bs) {
-            sb.append(byteToHex(b)).append(delimiter);
-        }
-        return sb.sub(0, sb.length() - delimiter.length());
-    }
-    
+
+    /**
+     * 将16进制字符串转为字节数组
+     *
+     * @param hex 16进制字符串
+     * @return byte[]字节数组
+     */
     public static byte[] hexToBytes(String hex) {
         if (Strings.isNull(hex)) {
             return EMPTY_BYTE;
@@ -86,66 +127,134 @@ public class Converts {
         }
         return bs;
     }
-    
-    public static boolean toBoolean(Object value) {
-        if (value == null) {return false;}
-        String t = Strings.toUpperCase(value);
-        return "TRUE".equals(t) || "Y".equals(t) || "YES".equals(t) || ("1".equals(t));
+
+    /**
+     * 将字节数组转为16进制字符串（大写），并用分割符分割
+     *
+     * @param bs        字节数组
+     * @param delimiter 分割符
+     * @return 如：0A,1B,2C
+     */
+    public static String bytesToHexWithDelimiter(byte[] bs, String delimiter) {
+        if (ArrayHelper.isEmpty(bs)) {
+            return "";
+        }
+        SB sb = New.sb();
+        for (byte b : bs) {
+            sb.append(byteToHex(b)).append(delimiter);
+        }
+        return sb.deleteLast(delimiter.length()).toString();
     }
-    
+
+    /**
+     * 将带分割符的16进制字符串解析成字节数组
+     *
+     * @param hex       带分隔符的16进制字符串，如：0A,1B,2C
+     * @param delimiter 分割符，如：“,” 或 “|” ……
+     * @return
+     */
+    public static byte[] hexWithDelimiterToBytes(String hex, String delimiter) {
+        // 判断字符串有效性
+        if (Strings.isNull(hex)) {
+            return EMPTY_BYTE;
+        }
+        // 判断分割符是否为空
+        if (Strings.isNotNull(delimiter)) {
+            // 判断分隔符是否需要转义
+            if ("|".equals(delimiter)) {
+                delimiter = "\\|";
+            }
+            String[] hexes = hex.split(delimiter);
+            byte[] bytes = new byte[hexes.length];
+            for (int i = 0; i < bytes.length; i++) {
+                if (onlyHex(hexes[i])) {
+                    bytes[i] = hexToByte(hexes[i]);
+                }
+            }
+            return bytes;
+        } else {
+            // 没有分隔符则直接解析
+            return hexToBytes(hex);
+        }
+
+    }
+
+    /**
+     * true，y，yes，1字符串值为true，其它为false
+     *
+     * @param value 需转换的值
+     * @return
+     */
+    public static boolean toBoolean(Object value) {
+        return toBoolean(value, false);
+    }
+
+    /**
+     * true，y，yes，1字符串值为true，其它为false
+     *
+     * @param value        需转换的值
+     * @param defaultValue 默认值
+     * @return
+     */
     public static boolean toBoolean(Object value, boolean defaultValue) {
-        if (value == null) {return defaultValue;}
+        if (value == null) {
+            return defaultValue;
+        }
         String t = Strings.toUpperCase(value);
         return "TRUE".equals(t) || "Y".equals(t) || "YES".equals(t) || ("1".equals(t)) || defaultValue;
     }
-    
-    public static boolean toBoolean(byte[] bs) {
-        return bs[0] != 0;
-    }
+
     /**
      * 将对象转为10进制int数值，转换出错时默认返回0
      *
      * @param value 转换对象
-     *
      * @return
      */
     public static int toInt(Object value) {
         return toInt(value, 0, 10);
     }
-    
+
     /**
      * 将对象转为10进制int数值，转换出错时默认返回defaultValue
      *
      * @param value        转换对象
      * @param defaultValue 默认值
-     *
      * @return
      */
     public static int toInt(Object value, int defaultValue) {
         return toInt(value, defaultValue, 10);
     }
-    
+
     /**
      * 将对象转为指定进制int数值，转换出错时默认返回defaultValue
      *
      * @param value        转换对象
      * @param defaultValue 默认值
      * @param radix        进制
-     *
      * @return
      */
     public static int toInt(Object value, int defaultValue, int radix) {
+        // 判断有效性
         if (value == null) {
+            // 为空返回默认值
             return defaultValue;
-        } else if (!value.getClass().equals(Integer.TYPE) && !(value instanceof Integer)) {
+        }
+        // 非int类型
+        if (!value.getClass().equals(Integer.TYPE) && !(value instanceof Integer)) {
+            // 非double类型
             if (!value.getClass().equals(Double.TYPE) && !(value instanceof Double)) {
+                // 转为字符串
                 String s = value.toString();
+                // 判断有效性
                 if (Strings.isNull(s)) {
+                    // 返回默认值
                     return defaultValue;
                 } else {
                     try {
+                        // 解析成对应进制的int值
                         return Integer.parseInt(s, radix);
                     } catch (Exception e) {
+                        // 异常返回默认值
                         return defaultValue;
                     }
                 }
@@ -155,20 +264,22 @@ public class Converts {
         } else {
             return (Integer) value;
         }
+
     }
-    
+
     public static byte toByte(Object value) {
         return toByte(value, (byte) 0, 10);
     }
-    
+
     public static byte toByte(Object value, byte defaultValue) {
         return toByte(value, defaultValue, 10);
     }
-    
+
     public static byte toByte(Object value, byte defaultValue, int radix) {
         if (value == null) {
             return defaultValue;
-        } else if (!(value instanceof Byte) && !(value instanceof Integer)) {
+        }
+        if (!(value instanceof Byte) && !(value instanceof Integer)) {
             String s = value.toString();
             if (Strings.isNull(s)) {
                 return defaultValue;
@@ -183,20 +294,22 @@ public class Converts {
         } else {
             return Byte.valueOf(value.toString());
         }
+
     }
-    
+
     public static short toShort(Object value) {
         return toShort(value, (short) 0, 10);
     }
-    
+
     public static short toShort(Object value, short defaultValue) {
         return toShort(value, defaultValue, 10);
     }
-    
+
     public static short toShort(Object value, short defaultValue, int radix) {
         if (value == null) {
             return defaultValue;
-        } else if (!value.getClass().equals(Short.TYPE) && !(value instanceof Short)) {
+        }
+        if (!value.getClass().equals(Short.TYPE) && !(value instanceof Short)) {
             if (!value.getClass().equals(Integer.TYPE) && !(value instanceof Integer)) {
                 if (!value.getClass().equals(Double.TYPE) && !(value instanceof Double)) {
                     String s = value.toString();
@@ -219,15 +332,16 @@ public class Converts {
             return (Short) value;
         }
     }
-    
+
     public static long toLong(Object value) {
         return toLong(value, 0L);
     }
-    
+
     public static long toLong(Object value, long defaultValue) {
         if (value == null) {
             return defaultValue;
-        } else if (!value.getClass().equals(Long.TYPE) && !(value instanceof Long)) {
+        }
+        if (!value.getClass().equals(Long.TYPE) && !(value instanceof Long)) {
             String s = value.toString();
             if (Strings.isNull(s)) {
                 return defaultValue;
@@ -242,15 +356,16 @@ public class Converts {
             return (Long) value;
         }
     }
-    
+
     public static float toFloat(Object value) {
         return toFloat(value, 0.0F);
     }
-    
+
     public static float toFloat(Object value, float defaultValue) {
         if (value == null) {
             return defaultValue;
-        } else if (!value.getClass().equals(Float.TYPE) && !(value instanceof Float)) {
+        }
+        if (!value.getClass().equals(Float.TYPE) && !(value instanceof Float)) {
             if (value instanceof Double) {
                 return ((Double) value).floatValue();
             } else {
@@ -269,15 +384,16 @@ public class Converts {
             return (Float) value;
         }
     }
-    
+
     public static double toDouble(Object value) {
         return toDouble(value, 0.0D);
     }
-    
+
     public static double toDouble(Object value, double defaultValue) {
         if (value == null) {
             return defaultValue;
-        } else if (!value.getClass().equals(Double.TYPE) && !(value instanceof Double)) {
+        }
+        if (!value.getClass().equals(Double.TYPE) && !(value instanceof Double)) {
             String s = value.toString();
             if (Strings.isNull(s)) {
                 return defaultValue;
@@ -292,27 +408,21 @@ public class Converts {
             return (Double) value;
         }
     }
-    
+
     public static BigDecimal toBigDecimal(Object value) {
         return toBigDecimal(value, (BigDecimal) null);
     }
-    
+
     public static BigDecimal toBigDecimal(Object value, BigDecimal defaultValue) {
         return value == null ? defaultValue : new BigDecimal(value.toString());
     }
-    
-    // /**
-    //  * 16进制字符串转为 byte[],如果包含其它字符(空格除外)，将返回空数组
-    //  *
-    //  * @param hex 16进制字符串
-    //  *
-    //  * @return byte[]
-    //  */
-    // public static byte[] hexToBytes(String hex) {
-    //     String s = Strings.removeSpaces(hex);
-    //     return onlyHex(s) ? getBytes(fixHex(s)) : EMPTY_BYTE;
-    // }
-    
+
+    /**
+     * 将可序列化对象转为字节数组
+     *
+     * @param serial 可序列化对象
+     * @return
+     */
     public static byte[] objectToBytes(Serializable serial) {
         if (serial == null) {
             return EMPTY_BYTE;
@@ -321,7 +431,6 @@ public class Converts {
                  ObjectOutputStream out = new ObjectOutputStream(bsOut)) {
                 out.writeObject(serial);
                 out.flush();
-                out.close();
                 return bsOut.toByteArray();
             } catch (IOException e) {
                 Logs.getLogger(Converts.class).error(Strings.getExceptionTrace(e));
@@ -329,53 +438,72 @@ public class Converts {
             }
         }
     }
-    
+
+    /**
+     * 将字节数组转为对象
+     *
+     * @param bytes 字节数组
+     * @param <T>   结果对象
+     * @return
+     */
+    public static <T> T bytesToObject(byte[] bytes, Class<T> clazz) {
+        if (ArrayHelper.isEmpty(bytes)) {
+            return null;
+        }
+        try (ByteArrayInputStream bsIn = new ByteArrayInputStream(bytes);
+             ObjectInputStream in = new ObjectInputStream(bsIn)) {
+            Object o = in.readObject();
+            return (T) o;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     // ------------------------ 私有方法 ------------------------
-    
+
     /**
      * 判断是否只包含16进制字符串
      *
      * @param check 需检查字符串
-     *
      * @return boolean
-     *
      * @author AD
      * @date 2018-12-22 18:37
      */
     private static boolean onlyHex(String check) {
         for (char c : check.toCharArray()) {
-            if (ALL_HEX.indexOf(c) == -1) return false;
+            if (ALL_HEX.indexOf(c) == -1) {
+                return false;
+            }
         }
         return true;
     }
-    
+
     /**
      * 修正16进制字符串长度，如：A --> 0A
      *
      * @param hex 需修正的16进制字符串
-     *
      * @return
      */
     private static String fixHex(String hex) {
         return hex.length() % 2 == 0 ? hex : "0" + hex;
     }
-    
+
     /**
      * 将修正长度(奇数长度前面补0)后的16进制字符串转为字节数组
      *
      * @param hex 16进制字符串
-     *
      * @return byte[]
      */
     private static byte[] getBytes(String hex) {
-        
+
         final char[] cs = hex.toUpperCase().toCharArray();
         byte[] bs = new byte[cs.length / 2];
-        
+
         for (int i = 0, k = 0, L = bs.length; i < L; ++i, k += 2) {
             bs[i] = (byte) (HEX.indexOf(cs[k]) << 4 | HEX.indexOf(cs[k + 1]));
         }
         return bs;
     }
-    
+
 }
--- src/main/java/com/x/doraemon/util/Strings.java
+++ src/main/java/com/x/doraemon/util/Strings.java
@@ -21,56 +21,55 @@ import java.util.stream.Stream;
  * @Author AD
  */
 public final class Strings {
-    
+
     // ------------------------ 变量定义 ------------------------
-    
+
     /**
      * 16进制字符串(大小写)
      */
     public static final String ALL_HEX = "0123456789ABCDEFabcdef";
-    
+
     /**
      * 空字符串
      */
     public static final String NULL = "";
-    
+
     /**
      * 16进制字符串(大写)
      */
     public static final String HEX = "0123456789ABCDEF";
-    
+
     /**
      * 大小写字母、数字
      */
     public static final char[] LETTER_FIGURE =
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
-    
+
     /**
      * 随机变量
      */
     private static final Random RANDOM = new Random();
     // ------------------------ 构造方法 ------------------------
-    
-    private Strings() {}
-    
+
+    private Strings() {
+    }
+
     // ------------------------ 方法定义 ------------------------
-    
+
     /**
      * 将对象转为字符串(禁止使用强制类型转换)
      *
      * @param o 转换对象
-     *
      * @return
      */
     public static String of(Object o) {
         return String.valueOf(o);
     }
-    
+
     /**
      * 将第一个字母改为大写
      *
      * @param s 需要修改的字符串
-     *
      * @return
      */
     public static String firstToUpper(String s) {
@@ -78,12 +77,11 @@ public final class Strings {
         bs[0] = (byte) ((char) bs[0] - 97 + 65);
         return new String(bs);
     }
-    
+
     /**
      * 将第一个字母改为小写
      *
      * @param s 需要修改的字符串
-     *
      * @return
      */
     public static String firstToLower(String s) {
@@ -91,14 +89,13 @@ public final class Strings {
         bs[0] = (byte) ((char) bs[0] - 65 + 97);
         return new String(bs);
     }
-    
+
     /**
      * 比较两个字符串是否相等
      *
      * @param one  第一个字符串
      * @param two  第二个字符串
      * @param trim 是否去除前后空格比较
-     *
      * @return
      */
     public static boolean equals(String one, String two, boolean trim) {
@@ -106,19 +103,18 @@ public final class Strings {
         two = two == null ? "" : (trim ? two.trim() : two);
         return one.equals(two);
     }
-    
+
     /**
      * byte[] --> 字符串，指定编码方式
      *
      * @param bs      byte[]
      * @param charset
-     *
      * @return String
      */
     public static String decode(byte[] bs, Charset charset) {
         return new String(bs, charset);
     }
-    
+
     /**
      * toString方法，默认多行显示<br>
      * 如：
@@ -128,93 +124,85 @@ public final class Strings {
      * ]
      *
      * @param o 需要重写toString方法的对象
-     *
      * @return
      */
     public static String defaultToString(Object o) {
         return o == null ? "<null>" : reflectToString(o, DisplayStyle.MULTI_LINE);
     }
-    
+
     /**
      * toString方法，简单类名+属性，单行显示<br>
      * 如：NameValue[name=name,value=AD]
      *
      * @param o 需要重写toString方法的对象
-     *
      * @return
      */
     public static String simpleToString(Object o) {
         return o == null ? "<null>" : reflectToString(o, DisplayStyle.SHORT_PREFIX);
     }
-    
+
     /**
      * 自定义风格的toString方法
      *
      * @param o     需要重写toString方法的对象
      * @param style 显示风格
-     *
      * @return
      */
     public static String reflectToString(Object o, DisplayStyle style) {
         return ToStringBuilder.reflectionToString(o, style.get());
     }
-    
+
     public static String toJsonString(Object o) {
-        return o.getClass().getName() + "@" + o.hashCode()+"=" + Jsons.toJson(o);
+        return o.getClass().getName() + "@" + o.hashCode() + "=" + Jsons.toJson(o);
     }
-    
+
     /**
      * 用参数内容替换占位符
      *
      * @param template 如：{0}是中国人,来自{1},{2}岁
      * @param params   需要替换的参数
-     *
      * @return
      */
     public static String replace(String template, Object... params) {
         return isNull(template) ? "" : MessageFormat.format(template, toStrings(params));
     }
-    
+
     /**
      * 判断字符串是否为"","    "
      *
      * @param check 需检查的字符串,默认去除前后空白条
-     *
      * @return boolean
      */
     public static boolean isNull(String check) {
         return isNull(check, true);
     }
-    
+
     /**
      * 判断字符串是否不为"","  "
      *
      * @param check 需检查的字符串,默认去除前后空白条
-     *
      * @return boolean
      */
     public static boolean isNotNull(String check) {
-        
+
         return !isNull(check, true);
     }
-    
+
     /**
      * 判断字符串是否为null或者""（包含"null","{}"）
      *
      * @param check 需检查的字符串
-     *
      * @return boolean
      */
     public static boolean isNullStr(String check) {
         return isNull(check, "null", "{}");
     }
-    
+
     /**
      * 判断字符串是否为null
      *
      * @param check 需要检查的字符串
      * @param trim  是否去除前后空格
-     *
      * @return
      */
     public static boolean isNull(String check, boolean trim) {
@@ -224,27 +212,25 @@ public final class Strings {
             return check == null || check.length() == 0;
         }
     }
-    
+
     /**
      * 判断字符串是否为null(或者被认为是null的字符串,不区分大小写)|""|"  "
      *
      * @param check 需检查的字符串
      * @param nulls 被认为是null的字符串(如：{},"null")
-     *
      * @return boolean
      */
     public static boolean isNull(String check, String... nulls) {
-        
+
         return isNull(check, false, nulls);
     }
-    
+
     /**
      * 判断字符串是否为null(或者被认为是null的字符串,不区分大小写)|""|"  "
      *
      * @param check      需检查的字符串
      * @param ignoreCase 是否忽略大小写
      * @param nulls      被认为是null的字符串(如：{},"null")
-     *
      * @return boolean
      */
     public static boolean isNull(String check, boolean ignoreCase, String... nulls) {
@@ -253,13 +239,12 @@ public final class Strings {
         }
         return isNull(check) || Stream.of(nulls).anyMatch(n -> check.equals(n));
     }
-    
+
     /**
      * 判断字符串是否与字符串数组都不相等(不去除空格比较)
      *
      * @param check  需要判断的字符串
      * @param values 对比的字符串数组
-     *
      * @return true:不相等；false:相等
      */
     public static boolean isNotEquals(String check, String... values) {
@@ -286,191 +271,175 @@ public final class Strings {
         }
         return true;
     }
-    
+
     /**
      * 判断是否是本地IP
      *
      * @param ip          需要判断的IP
      * @param removeSpace 是否移除所有空格
-     *
      * @return
      */
     public static boolean isLocalhost(String ip, boolean removeSpace) {
         ip = removeSpace ? removeSpaces(ip) : ip;
         if (!isNull(ip)) {
             return ip.replaceAll("[0\\.]", NULL).length() == 0 ||
-                   "127.0.0.1".equals(ip) ||
-                   "localhost".equals(ip);
+                    "127.0.0.1".equals(ip) ||
+                    "localhost".equals(ip);
         }
         return true;
     }
-    
+
     /**
      * 是否URL格式
      *
      * @param check 需检验的字符串
-     *
      * @return
      */
     public static boolean isURLFormat(String check) {
         return match(check, Regex.URL);
     }
-    
+
     /**
      * 是否邮箱格式
      *
      * @param check
-     *
      * @return
      */
     public static boolean isEmailFormat(String check) {
         return match(check, Regex.EMAIL);
     }
-    
+
     /**
      * 是否英文
      *
      * @param check
-     *
      * @return
      */
     public static boolean isOnlyEnglish(String check) {
         return match(check, Regex.ONLY_ENGLISH);
     }
-    
+
     /**
      * 是否整数
      *
      * @param check
-     *
      * @return
      */
     public static boolean isLong(String check) {
         return match(check, Regex.LONG);
     }
-    
+
     /**
      * 是否数字，包括小数
      *
      * @param check
-     *
      * @return
      */
     public static boolean isNumeric(String check) {
         return match(check, Regex.NUMERIC);
     }
-    
+
     /**
      * 是否无符号数
      *
      * @param check
-     *
      * @return
      */
     public static boolean isUnsignedNumeric(String check) {
         return match(check, Regex.UNSIGNED_NUMERIC);
     }
-    
+
     /**
      * 是否浮点数，包括float，不包括整数
      *
      * @param check
-     *
      * @return
      */
     public static boolean isDouble(String check) {
         return match(check, Regex.DOUBLE) && !isLong(check);
     }
-    
+
     /**
      * 是否时间
      *
      * @param check
-     *
      * @return
      */
     public static boolean isTime(String check) {
         return match(check, Regex.TIME);
     }
-    
+
     /**
      * 是否日期
      *
      * @param check
-     *
      * @return
      */
     public static boolean isDate(String check) {
         return match(check, Regex.DATE);
     }
-    
+
     /**
      * 是否日期时间
      *
      * @param check
-     *
      * @return
      */
     public static boolean isDateTime(String check) {
         return match(check, Regex.DATE_TIME);
     }
-    
+
     public static boolean isDateOrTime(String check) {
         return match(check, Regex.DATE_OR_TIME);
     }
-    
+
     /**
      * 是否只包含中文，不能包含数字、标点
      *
      * @param check
-     *
      * @return
      */
     public static boolean isOnlyChinese(String check) {
         return match(check, Regex.ONLY_CHINESE);
     }
-    
+
     /**
      * 是否存在中文
      *
      * @param check
-     *
      * @return
      */
     public static boolean isExistChinese(String check) {
         return match(check, Regex.CHINESE);
     }
-    
+
     /**
      * 修正字符串长度为12，不足时以前缀"0"填充，否则截取
      *
      * @param fix 需修正的字符串
-     *
      * @return
      */
     public static String fix(String fix) {
         return fix(fix, 12, "0");
     }
-    
+
     /**
      * 修正字符串至固定长度，不足时以前缀"0"填充，否则截取
      *
      * @param fix         需修正的字符串
      * @param totalLength 修正后总长度
-     *
      * @return
      */
     public static String fix(String fix, int totalLength) {
         return fix(fix, totalLength, "0");
     }
-    
+
     /**
      * 修正字符串至固定长度，不足以前缀填充，一般是0
      *
      * @param fix         需修正的字符串
      * @param totalLength 修正后总长度
      * @param prefix      长度不足时以前缀填充，默认是0
-     *
      * @return
      */
     public static String fix(String fix, int totalLength, String prefix) {
@@ -488,12 +457,11 @@ public final class Strings {
         }
         return sb.sub(len - totalLength);
     }
-    
+
     /**
      * 获取异常信息
      *
      * @param throwable 可抛出的异常对象
-     *
      * @return
      */
     public static String getExceptionTrace(Throwable throwable) {
@@ -505,13 +473,12 @@ public final class Strings {
         throwable.printStackTrace(pw);
         return sw.toString();
     }
-    
+
     /**
      * 获取指定长度的随机字符串，包含数字、大小写字母
      *
      * @param length     指定长度
      * @param onlyFigure 是否只包含数字
-     *
      * @return
      */
     public static String getRandom(int length, boolean onlyFigure) {
@@ -528,7 +495,7 @@ public final class Strings {
         }
         return sb.get();
     }
-    
+
     /**
      * 移除字符串的所有空格
      *
@@ -538,31 +505,25 @@ public final class Strings {
         if (isNull(fix)) {
             return "";
         }
-        SB sb = New.sb();
-        for (char c : fix.toCharArray()) {
-            if (!Character.isSpaceChar(c)) sb.append(c);
-        }
-        return sb.get();
+        return fix.replaceAll("\\s+", "");
     }
-    
+
     /**
      * 转换成大写
      *
      * @param convert
-     *
      * @return
      */
     public static String toUpperCase(Object convert) {
         return convert == null ? "" : String.valueOf(convert).toUpperCase();
     }
     // ------------------------ 私有方法 ------------------------
-    
+
     /**
      * 正则表达式匹配
      *
      * @param check 需要检验的字符串
      * @param regex 正则表达式
-     *
      * @return
      */
     private static boolean match(String check, String regex) {
@@ -574,12 +535,11 @@ public final class Strings {
         }
         return false;
     }
-    
+
     /**
      * 将对象数组转为字符串
      *
      * @param os 对象数组
-     *
      * @return String[] 字符串数组
      */
     private static String[] toStrings(Object... os) {
@@ -592,5 +552,5 @@ public final class Strings {
         }
         return ss;
     }
-    
+
 }
